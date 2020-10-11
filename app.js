76bda8a2497676c8a39c29848a7669bb8a1b9bc1
Boris Ovčjak <boris.ovcjak@gmail.com>
2020-10-11 20:29:34 +0200

    Implemented slider progress on click

--- app.js
+++ app.js
@@ -3,10 +3,18 @@ const canvas = document.getElementById('canvas');
 const ctx = canvas.getContext('2d');
 
 const center = {};
+const currentLocation = {};
+
 const step = 50;
 const maxValue = 1000;
 
-const degree2Radian = degrees => degrees * (Math.PI / 180);
+// degrees to radians with -90 degree offset as circle begins at 3 o'clock
+const degree2Radian = degrees => (degrees - 90) * (Math.PI / 180);
+
+document.addEventListener('click', e => {
+  currentLocation.x = e.clientX;
+  currentLocation.y = e.clientY;
+})
 
 window.addEventListener('load', _ => {
   canvas.width = canvas.clientWidth;
@@ -19,7 +27,10 @@ window.addEventListener('load', _ => {
 })
 
 function render() {
+  ctx.clearRect(0, 0, canvas.width, canvas.height);
 
+  // Angle between center and current position in rad
+  const theta = Math.atan2(currentLocation.y - center.y, currentLocation.x - center.x);
   const gap = (step * 360) / maxValue;
 
   for(let i = 0; i < 360; i += gap) {
@@ -31,5 +42,12 @@ function render() {
     ctx.stroke();
     ctx.closePath();
   }
+
+  ctx.beginPath();
+  ctx.arc(center.x, center.y, 300, degree2Radian(0), theta);
+  ctx.strokeStyle = "green";
+  ctx.lineWidth = 40;
+  ctx.stroke();
   
+  window.requestAnimationFrame(render);
 }
